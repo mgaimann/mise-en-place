6d0da17553fb3c2b402ba8af017f8eba460288cc
Mario U. Gaimann <35263162+mu-gaimann@users.noreply.github.com>
2022-08-15 19:13:10 +0200

    added updating / upgrading

--- install_core.sh
+++ install_core.sh
@@ -1,23 +1,27 @@
 #!/bin/bash
 # script for installing several utilities
 
+### UPDATE EXISTING TOOLS
+sudo apt update -y
+sudo apt upgrade -y
+
 ### TEXT EDITOR
-sudo apt-get install vim
+sudo apt-get install vim -y
 
 ### BUILDING TOOLS
-sudo apt-get install cmake
+sudo apt-get install cmake -y
 
 ### FILE PROCESSING
-sudo apt-get install unzip
-sudo apt-get install pdfunite
+sudo apt-get install unzip -y
+sudo apt-get install pdfunite -y
 
 ### TERMINAL EFFICIENCY
-sudo apt-get install tmuxinator
+sudo apt-get install tmuxinator -y
 
 ### DOCUMENTATION GENERATION
-sudo apt-get install doxygen
+sudo apt-get install doxygen -y
 
 ### AESTHETICS
 # install jq command line processor
 # from: https://stedolan.github.io/jq/download/
-sudo apt-get install jq
+sudo apt-get install jq -y
