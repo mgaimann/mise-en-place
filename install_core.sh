f72121cdd707235bc1c1192bfe04c43ef5932d32
mu-gaimann <35263162+mu-gaimann@users.noreply.github.com>
2022-08-15 18:50:14 +0200

    added tmuxinator installation

--- install_core.sh
+++ install_core.sh
@@ -2,8 +2,14 @@
 
 # script for installing several utilities
 
+# install vim
+sudo apt-get install vim
+
 # install unzip
-sudo apt install unzip
+sudo apt-get install unzip
+
+# install tmux
+sudo apt-get install tmuxinator
 
 # install jq command line processor
 # from: https://stedolan.github.io/jq/download/
---
+++ install_tmuxinator.fish
@@ -0,0 +1,9 @@
+#!/usr/bin/fish
+
+
+
+# install completion files
+# from: https://github.com/tmuxinator/tmuxinator
+wget https://raw.githubusercontent.com/tmuxinator/tmuxinator/master/completion/tmuxinator.fish ~/.config/fish/completions/
+
+
