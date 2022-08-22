3ad3af6adba144916d477561978b619c046faf8d
mu-gaimann <35263162+mu-gaimann@users.noreply.github.com>
2022-08-22 17:01:53 +0200

    fixed a conda/fish error

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,3 +1,13 @@
-#!/usr/bin/fish
+if status is-interactive
+    # Commands to run in interactive sessions can go here
 
+# note: conda init must be inside status is-interactive
+# source: https://github.com/jorgebucaran/hydro/issues/6#issuecomment-761981523
+
+# >>> conda initialize >>>
+# !! Contents within this block are managed by 'conda init' !!
+eval /home/mario/anaconda3/bin/conda "shell.fish" "hook" $argv | source
+# <<< conda initialize <<<
+
+end
 
