117796e44f8e527483454158d27302e421bb6387
mu-gaimann <35263162+mu-gaimann@users.noreply.github.com>
2022-08-22 10:33:19 +0200

    fixed tmuxinator config script

--- install_tmuxinator.fish
+++ install_tmuxinator.fish
@@ -1,9 +1,10 @@
 #!/usr/bin/fish
 
-
+# install tmuxinator
+sudo apt-get install tmuxinator -y
 
 # install completion files
 # from: https://github.com/tmuxinator/tmuxinator
-wget https://raw.githubusercontent.com/tmuxinator/tmuxinator/master/completion/tmuxinator.fish ~/.config/fish/completions/
+wget https://raw.githubusercontent.com/tmuxinator/tmuxinator/master/completion/tmuxinator.fish -P ~/.config/fish/completions/
 
 
