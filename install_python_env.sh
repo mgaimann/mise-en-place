721b5844a16e9789df9cc60743380011599e8d05
mu-gaimann <35263162+mu-gaimann@users.noreply.github.com>
2022-08-22 10:28:04 +0200

    changed installation from fish to bash script

---
+++ install_python_env.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/bash
+
+# installation script for creating the python environment
+
+# install mamba
+curl -L -O "https://github.com/conda-forge/miniforge/releases/latest/download/Mambaforge-Linux-x86_64.sh"
+bash Mambaforge-Linux-x86_64.sh
+rm Mambaforge-Linux-x86_64.sh
+
+# add conda to fish config
+conda init fish
