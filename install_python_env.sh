3d02cbe2e594f6fb10653f0abd02b7d03625ac7d
Mario U. Gaimann <35263162+mu-gaimann@users.noreply.github.com>
2022-10-12 13:28:37 +0200

    source .bashrc before using conda

--- install_python_env.sh
+++ install_python_env.sh
@@ -6,6 +6,7 @@
 curl -L -O "https://github.com/conda-forge/miniforge/releases/latest/download/Mambaforge-Linux-x86_64.sh"
 bash Mambaforge-Linux-x86_64.sh
 rm Mambaforge-Linux-x86_64.sh
+source ~/.bashrc
 
 # add conda to fish config
 conda init fish
