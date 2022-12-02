987881896f4c349f28325fda9237c228b56a4dd6
mu-gaimann <35263162+mu-gaimann@users.noreply.github.com>
2022-12-02 10:54:06 +0100

    added draft for astro nvim install

---
+++ install_astro_nvim.fish
@@ -0,0 +1,7 @@
+# astroNvim
+# following these steps: https://astronvim.github.io/#%EF%B8%8F-installation
+
+mv ~/.config/nvim ~/.config/nvim.bak
+mv ~/.local/share/nvim/site ~/.local/share/nvim/site.bak
+git clone https://github.com/AstroNvim/AstroNvim ~/.config/nvim
+nvim
