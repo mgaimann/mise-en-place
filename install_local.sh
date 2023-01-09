a641f03c85dbddfa5d49455e3d5ca888472bfcfd
mu-gaimann <35263162+mu-gaimann@users.noreply.github.com>
2023-01-09 10:56:47 +0100

    moved appimages manger from core to local

--- install_local.sh
+++ install_local.sh
@@ -9,3 +9,10 @@ sudo apt-get install jq -y
 ### BATTERY SAVING / FAN CONTROL
 sudo apt-get install tlp
 sudo tlp-stat -s
+
+### APP IMAGES MANAGER
+# from: https://github.com/TheAssassin/AppImageLauncher/wiki/Install-on-Ubuntu-or-Debian
+sudo apt-get install software-properties-common -y
+sudo add-apt-repository ppa:appimagelauncher-team/stable -y
+sudo apt-get update -y
+sudo apt-get install appimagelauncher -y
