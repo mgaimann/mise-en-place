02abf19332d10cd46d55aecdd39e7475c8ca34dd
mgaimann <gaimannscience@gmail.com>
2024-02-05 17:14:35 +0100

    changed conda init

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -4,10 +4,13 @@ if status is-interactive
 # note: conda init must be inside status is-interactive
 # source: https://github.com/jorgebucaran/hydro/issues/6#issuecomment-761981523
 
-# >>> conda initialize >>>
-# !! Contents within this block are managed by 'conda init' !!
-eval /datapool/homes/ac141173/mambaforge/bin/conda "shell.fish" "hook" $argv | source
-# <<< conda initialize <<<
+    export PATH="/home/ac141173/mambaforge/bin:$PATH"
+
+    # >>> conda initialize >>>
+    # !! Contents within this block are managed by 'conda init' !!
+    eval /home/ac141173/mambaforge/bin/conda "shell.fish" "hook" $argv | source
+    # <<< conda initialize <<<
 
 end
+
 
