0202d7c5b1dd96f9425901abea0ca797cbc88e12
mgaimann <gaimannscience@gmail.com>
2024-02-05 17:13:45 +0100

    increase robustness of script

--- install_python_env.sh
+++ install_python_env.sh
@@ -4,7 +4,7 @@
 
 # install mamba
 curl -L -O "https://github.com/conda-forge/miniforge/releases/latest/download/Mambaforge-Linux-x86_64.sh"
-bash Mambaforge-Linux-x86_64.sh
+bash Mambaforge-Linux-x86_64.sh -u
 rm Mambaforge-Linux-x86_64.sh
 source ~/.bashrc
 
